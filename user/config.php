64eb39cd697f1a5de7aafb93859f2a4c0315d761
Zaemond <myanmargaming78@gmail.com>
2025-11-19 20:35:24 +0630

    Add files via upload

---
+++ user/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Change this to your connection info.
+$DATABASE_HOST = 'db_host';
+$DATABASE_USER = 'db_user';
+$DATABASE_PASS = 'db_pass';
+$DATABASE_NAME = 'db_name';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$mysql = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+?>
+
+
